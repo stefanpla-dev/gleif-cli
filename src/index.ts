b6f021a2c2e86e9ad673fc2149d4da34f1240a7e
Stefan Pla <spla@icloud.com>
2026-01-27 15:07:10 -0500

    let user select a validated specific entity when multiple are returned

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-// console.log("hello world");
 import { searchByName } from "./gleif.js";
+import * as readline from 'readline';
+
+function prompt(question: string): Promise<string> {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            rl.close();
+            resolve(answer);
+        });
+    });
+}
 
 async function main() {
     if (process.argv.length < 3) {
@@ -10,9 +24,16 @@ async function main() {
         const records = await searchByName(process.argv[2]!);
         records.forEach((record, i) => {
             console.log(`${i + 1}. ${record.legalName} (${record.lei}) - ${record.status}`);
+            // Eventually format the output as a table.
         })
+        const answer = await prompt('Enter the number corresponding to the desired entity: ');
+        const index = parseInt(answer) - 1;
+        if (isNaN(index) || index < 0 || index >= records.length) {
+            console.error("Invalid selection.");
+            process.exit(1);
+        }
+        console.log('You selected:', records[index]);
     }
-
 }
 
 main();
