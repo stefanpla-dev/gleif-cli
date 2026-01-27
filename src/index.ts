b45d23ee50e3491f58fa5b37741f7af5462cd197
Stefan Pla <spla@icloud.com>
2026-01-27 12:58:04 -0500

    add command line argument functionality and error messaging.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@
 import { searchByName } from "./gleif.js";
 
 async function main() {
-    const records = await searchByName('Apple');
-    console.log(records);
+    if (process.argv.length < 3) {
+        console.error("Usage: tsx src/index.ts <legal-name>");
+        process.exit(1);
+    } else {
+        console.log(`Searching for LEI records with legal name: ${process.argv[2]}`);
+        const records = await searchByName(process.argv[2]!);
+        console.log(records);
+    }
+
 }
 
 main();
