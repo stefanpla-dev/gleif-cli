2b09d9787deb44713e739ec1be54fc4a1edfe86d
Stefan Pla <spla@icloud.com>
2026-01-27 11:48:32 -0500

    create searchByName function

--- src/gleif.ts
+++ src/gleif.ts
@@ -15,3 +15,19 @@ interface GleifSearchResponse {
         };
     }[];
 }
+
+export async function searchByName(name: string): Promise<LeiRecord[]> {
+    const url = `https://api.gleif.org/api/v1/lei-records?filter[entity.legalName]=${name}`;
+    const response = await fetch(url);
+    const json: GleifSearchResponse = await response.json();
+
+    const records = json.data.map((item) => {
+        return {
+            lei: item.attributes.lei,
+            legalName: item.attributes.entity.legalName.name,
+            status: item.attributes.entity.status,
+        };
+    });
+
+    return records;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 // console.log("hello world");
 
-async function main () {
-    const response = await fetch("https://api.gleif.org/api/v1/lei-records?filter[entity.legalName]=Apple ")
-    const data = await response.json();
+async function main() {
+    const response = await fetch("https://api.gleif.org/api/v1/lei-records?filter[entity.legalName]=Apple");
+    const data = response.json(); 
     console.log(data);
+
 }
 
-main ();
+main();
 
